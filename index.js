11ecfc226a7ae4ec5706dc5f7c93bfe080c427d6
Kallel Roman <kallelr@outlook.com>
2019-07-28 23:04:35 -0300

    prefix and composed arguments

--- index.js
+++ index.js
@@ -5,32 +5,34 @@ const client = new Discord.Client();
 
 const token = process.env.DISCORD_TOKEN;
 
+const PREFIX = "!"
+
 client.on('ready', () =>{
     console.log('Bot is on');
 });
 
-client.on('message', msg=>{
-    if(msg.content === "HELLO"){
-        msg.reply('e ae corno');
+client.on('message', message=>{
+    let args = message.content.substring(PREFIX.lenght).split(" ");
+
+    switch (args[0]){
+        case PREFIX+'ping':
+            message.channel.send('pong');
+            break;
+        case PREFIX+'website':
+            message.channel.send('github.com/kallelrm');
+            break;
+        case PREFIX+'info':
+            if(args[1]==="version"){
+                message.channel.send("0.0.1");
+            }else{
+                message.channel.send("Invalid Args");
+            }
+        break;
+        case PREFIX+'clear':
+            if(!args[1]) return message.reply('Please define a second arg')
+            message.channel.bulkDelete(args[1]);
+            break;
     }
 });
-
-client.on('message', msg=>{
-    if(msg.content === "88"){
-        msg.reply('O único erro de Hitler foi não matar todos os judeus');
-    }
-});
-
-client.on('message', msg=>{
-    if(msg.content === "negro"){
-        msg.reply('eu não sou racista, racismo da cadeia e cadeia é lugar de preto');
-    }
-})
-
-client.on('message', msg=>{
-    if(msg.content === "dilema"){
-        msg.reply("tu daria o cu pro teu pai pra não comer a tua mãe?");
-    }
-})
 
 client.login(token);
